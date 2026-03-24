654a6e49b40596dcf470ab71d77fed80b66da128
Dipen <dipen.sejpal26@gmail.com>
2026-03-24 16:08:02 +0530

    fix issue

--- frontend/src/auth/AuthService.ts
+++ frontend/src/auth/AuthService.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1/auth';
+const API_AUTH_URL = import.meta.env.VITE_API_AUTH_URL || 'http://localhost:8000/api/v1/auth';
 
 export const AuthService = {
     login: async (email: string, password: string) => {
-        const response = await axios.post(`${API_URL}/login`, { email, password });
+        const response = await axios.post(`${API_AUTH_URL}/login`, { email, password });
         if (response.data.access) {
             localStorage.setItem('user', JSON.stringify(response.data));
         }
