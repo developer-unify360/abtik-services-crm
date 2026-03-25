1d62a7eb3e577e4b57a0d4992c94c23b935d54b9
Dipen <dipen.sejpal26@gmail.com>
2026-03-25 11:26:23 +0530

    fix design

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -3,21 +3,19 @@ import Sidebar from './Sidebar';
 import HeaderBar from './HeaderBar';
 import { Outlet } from 'react-router-dom';
 
-const DRAWER_WIDTH = 200; // More compact sidebar width
+const DRAWER_WIDTH = 240;
 
 const Layout: React.FC = () => {
   return (
-    <div className="flex viewport-height bg-gray-50 overflow-hidden">
+    <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
       <div 
-        className="flex-1 flex flex-col min-w-0"
+        className="flex-1 ml-60"
         style={{ marginLeft: `${DRAWER_WIDTH}px` }}
       >
         <HeaderBar />
-        <main className="flex-1 overflow-hidden p-1.5 pt-0">
-          <div className="h-full overflow-hidden">
-            <Outlet />
-          </div>
+        <main className="pt-16 px-3 pb-3">
+          <Outlet />
         </main>
       </div>
     </div>
